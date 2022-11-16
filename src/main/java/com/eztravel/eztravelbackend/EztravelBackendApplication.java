24b757ea3a2b05c58998a5473bf31cedee8f3986
vicki962 <levictoria1.jackson@famu.edu>
2022-11-17 01:02:25 +0530

    first commit

---
+++ src/main/java/com/eztravel/eztravelbackend/EztravelBackendApplication.java
@@ -0,0 +1,13 @@
+package com.eztravel.eztravelbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EztravelBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EztravelBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eztravel/eztravelbackend/EztravelBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eztravel.eztravelbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EztravelBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
